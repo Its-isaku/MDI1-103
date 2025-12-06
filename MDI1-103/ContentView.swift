a5aced74eb00fb97969f3a67f81d1b3e63243861
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2025-12-06 12:31:41 -0800

    Initial Commit

---
+++ MDI1-103/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDI1-103
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 06/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDI1-103/MDI1_103App.swift
@@ -0,0 +1,17 @@
+//
+//  MDI1_103App.swift
+//  MDI1-103
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MDI1_103App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
